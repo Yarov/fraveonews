843a960ad38f4a82c06f40961b40fc21f8a0c71b
Yarov <yarovrr@gmail.com>
2021-01-03 10:08:55 -0600

    init app add post by user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :promotions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :masqueradable, :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  scope :dashboard do
+    get '/', to: 'dashboard#index', as: 'dashboard'
+    resources :promotions
+  end
+  namespace :admin do
+    resources :users
+    resources :announcements
+    resources :notifications
+    resources :services
+
+    root to: "users#index"
+  end
+
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
     authenticate :user, lambda { |u| u.admin? } do
@@ -8,9 +21,15 @@ Rails.application.routes.draw do
     end
 
 
+  # api
+  namespace :api do
+    resources :promotions, only: [:index]
+  end
+  
+  root to: 'home#index'
+
   resources :notifications, only: [:index]
   resources :announcements, only: [:index]
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-  root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
